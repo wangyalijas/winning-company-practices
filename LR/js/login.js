b1ba315a622d0f892694f8417156ca5dc4d25cf7
wangyalijas <1114920411@qq.com>
2017-05-12 15:45:09 +0800

    2

--- LR/js/login.js
+++ LR/js/login.js
@@ -125,8 +125,14 @@ function login() {
 function register() {
 
     var phone_number = document.querySelector('#phone_number');
+    var verify_code = document.querySelector('#verify_code');
+    var password = document.querySelector('#password');
     if (!checkPhoneNumber(phone_number)) {
         return false;
+    }
+
+    if (password.value.length < 6 || password.value.length > 12) {
+        alert('密码格式错误，长度在6-12之间');
     }
 
     var user = findUserViaPhoneNumber(phone_number.value);
@@ -136,12 +142,26 @@ function register() {
 
     } else {
 
+        if (!checkVerifyCode(phone_number.value, verify_code.value)) {
+            alert('验证码错误');
+        } else {
+
+            var users = getUsers();
+            users.push({
+                phone_number: phone_number.value,
+                password: password.value
+            });
+            saveStorage('users', users);
+            alert('注册成功');
+            // 跳转页面
+        }
 
     }
 
 }
 
-function sendVerifyCode() {
+function sendVerifyCode() {//发送验证码
+    console.log('seed verify code function.');
 
     var phone_number = document.querySelector('#phone_number');
     if (!checkPhoneNumber(phone_number)) {
@@ -154,20 +174,63 @@ function sendVerifyCode() {
     var verifying_user_index = _.findIndex(verifying_users, ['phone_number', phone_number.value]);
     if (verifying_user_index < 0) {
         console.log(verifying_users, 'push');
+        var verify_code = rand(1000, 9999)
         verifying_users.push({
             phone_number: phone_number.value,
-            verify_code: rand(1000, 9999)
+            verify_code: verify_code
         });
         console.log(verifying_users, 'pushed');
-        console.log(verifying_users[0].verify_code)
+        console.log(verify_code)
     } else {
         verifying_users[verifying_user_index].verify_code = rand(1000, 9999);
     }
     saveStorage('verifying_users', verifying_users);
+    alert('验证码发送成功');
+
+}
+
+
+function reasetCode() {
 
 }
 
 function rand(n, m) {
     var c = m - n + 1;
     return Math.floor(Math.random() * c + n);
+}
+
+function getVerifyingUsers() {
+
+    var verifying_users = localStorage.getItem('verifying_users');
+    verifying_users = stringToJson(verifying_users, []);
+    return verifying_users;
+
+}
+
+function checkVerifyCode(phone_number, verify_code) {
+    console.log('check verify code function.');
+
+    var verifying_users = getVerifyingUsers();
+    var verify_user_index = _.findIndex(verifying_users, ['phone_number', phone_number]);
+
+    //如果大于等于0说明已经发送过验证码，如果小于0则直接返回false验证失败
+    console.log(verify_user_index, 'verify user index');
+    if (verify_user_index >= 0) {
+        //获取正在验证的用户
+        var verify_user = verifying_users[verify_user_index];
+
+        //验证是否正确
+        console.log(
+            verify_user.verify_code,
+            Number(verify_code),
+            verify_user.verify_code === Number(verify_code)
+        );
+        if (verify_user.verify_code === Number(verify_code)) {
+            // 验证成功
+            return true;
+        }
+    }
+
+    // 如果走到这一步说明验证都没进去，就是验证失败
+    return false;
 }
